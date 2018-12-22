7ccbf6c4163dccdfd0919be96108af5e7210f462
pedrubiano <pedrubiano@gmail.com>
2018-12-22 17:33:47 -0500

    Update index.php

--- res/validacion/index.php
+++ res/validacion/index.php
@@ -11,12 +11,12 @@
 	$usuario = new Usuario();
 	$usuario->where("A.usuario = '".$_POST['usuario']."'");
 	//$usuario->where("B.fecha_caducidad <= '".$hoy."'");
-	$usuario->setJoin("B.DB","empresas B","B.id = A.id_empresa AND B.DB = '".$data[1]."'");
+	$usuario->setJoin("B.DB, B.fecha_caducidad","empresas B","B.id = A.id_empresa AND B.DB = '".$data[1]."'");
 	$usuario->limit(1);
 
 	if($usu = $usuario->selectAll()) {
 		$_SESSION['id_empresa']=$usu[0]['id_empresa'];
-		if ($usu[0]['fecha_caducidad']>$hoy)
+		if ($usu[0]['fecha_caducidad'] >= $hoy)
 			header("Location:../../../app/res/validacion/?usuario=".$data[0]."&clave=".$_POST["clave"]."&empresa=".$data[1]);
 		else {
 			header("Location:../../../account/res/validacion/?key=".$data[0]."&empresa=".$data[1]);
