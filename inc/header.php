7ec68dffb1a1719502b4778905202de5e76abbc3
pedrubiano <pedrubiano@gmail.com>
2019-01-03 09:33:07 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -17,6 +17,8 @@
       <title>Logis | <? echo $modulo!='' ? $modulo : "Bienvenido";?></title>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+      <link rel="shortcut icon" href="<?echo $dir;?>/img/favicon.png" type="image/x-icon"/>
+
       <!-- Bootstrap CSS -->
       <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
       <link rel="stylesheet" href="<?echo $dir;?>/css/bootstrap/bootstrap.min.css" >
