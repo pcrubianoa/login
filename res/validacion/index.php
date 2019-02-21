c99991e7f7f9d6eaf7e571eefa297a2f8afefee0
pedrubiano <pedrubiano@gmail.com>
2019-02-21 18:02:13 -0500

    Update index.php

--- res/validacion/index.php
+++ res/validacion/index.php
@@ -6,7 +6,7 @@
 
 	$data = explode('.', $_POST['usuario']);
 
-	$hoy = date('Y-m-d');
+	$hoy = strtotime(date('Ymd'));
 
 	$usuario = new Usuario();
 	$usuario->where("A.usuario = '".$_POST['usuario']."'");
@@ -16,7 +16,12 @@
 
 	if($usu = $usuario->selectAll()) {
 		$_SESSION['id_empresa']=$usu[0]['id_empresa'];
-		if ($usu[0]['fecha_caducidad'] >= $hoy)
+
+		$venc = strtotime($usu[0]['fecha_caducidad']);
+		$diff = round(($venc-$hoy) / 86400); 
+		$_SESSION['limit_days'] = $diff;
+
+		if ($diff > 0)
 			header("Location:../../../app/res/validacion/?usuario=".$data[0]."&clave=".$_POST["clave"]."&empresa=".$data[1]);
 		else {
 			header("Location:../../../account/res/validacion/?key=".$data[0]."&empresa=".$data[1]);
